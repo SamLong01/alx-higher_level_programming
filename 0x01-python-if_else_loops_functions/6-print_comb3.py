ea4af0b37cdbafdccdea237c5017f119441063b2
SamLong01 <samueladebayo1015@gmail.com>
2022-12-01 04:57:35 +0100

    if/else

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
-    if int(i / 10) != i % 10 and int(i / 10) < i % 10:
-        print("{}{}".format(int(i / 10), i % 10), end="")
-        if (i != 89):
-            print(", ", end="")
-print("")
+for i in range(10):
+    for j in range(i+1, 10):
+        if i == 8 and j == 9:
+            print("{1}{0}".format(j, i))
+        else:
+            print("{}{}".format(i, j), end=", ")
